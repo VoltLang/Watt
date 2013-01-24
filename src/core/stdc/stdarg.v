8e1b8c6b943a488a9a35a9f50a4aa493941a6064
Jakob Bornecrantz <wallbraker@gmail.com>
2013-01-24 16:12:33 +0100

    stdc: Add stdio

---
+++ src/core/stdc/stdarg.v
@@ -0,0 +1,10 @@
+// Copyright © 2013, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/watt/license.d (BOOST ver. 1.0).
+module core.stdc.stdarg;
+
+
+extern(C):
+@system: // Types only.
+nothrow:
+
+alias va_list = void*;
